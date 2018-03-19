406fcf36d0a5070c0122e3cf2538a5c86912af18
Glitch (hello-express) <none>
2018-03-19 21:14:04 +0000

    🍕🌟 Checkpoint ./views/index.html:96831/368 ./public/style.css:96831/36 ./server.js:96831/437

--- server.js
+++ server.js
@@ -16,23 +16,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
