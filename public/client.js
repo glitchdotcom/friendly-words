1405803426f1f35dbfdd8be7497dc27ff6b8dead
Glitch (hello-express) <none>
2018-04-09 21:22:55 +0000

    🌦🏏 Checkpoint ./public/client.js:96831/492

--- public/client.js
+++ public/client.js
@@ -43,26 +43,3 @@
   };
   
 })()
-
-/*
-$(function() {
-  console.log('hello world :o');
-  
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
-  });
-
-  $('form').submit(function(event) {
-    event.preventDefault();
-    var dream = $('input').val();
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
-  });
-
-});
-*/
