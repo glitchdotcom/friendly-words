4f3e815d30ba25f562d3848ccbee1b54d96f5ee8
Glitch (hello-express) <none>
2018-06-29 19:22:21 +0000

    🌔😺 Checkpoint ./server.js:334152/7

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = express();
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
+app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
