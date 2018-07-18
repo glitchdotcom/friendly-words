6c7cefe9a2cdb523117b8685de2ea06c64d26f34
Glitch (hello-express) <none>
2018-07-18 21:18:27 +0000

    👗🎓 Checkpoint ./server.js:334152/1

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.get('/', function(request, response) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
