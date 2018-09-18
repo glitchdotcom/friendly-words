61086fa4b5805f1bef0b7483c4b34fada1a4e35c
Glitch (friendly-words) <none>
2018-09-18 20:05:24 +0000

    🌱🐪 Checkpoint ./words/normalize.js:8/2188

--- words/normalize.js
+++ words/normalize.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const Set = Set; // <--
 
 console.log("Starting Normalize");
 for(let fileName of ["objects.txt", "predicates.txt"]) {
@@ -6,10 +7,32 @@ for(let fileName of ["objects.txt", "predicates.txt"]) {
   const contents = fs.readFileSync(__dirname + `${fileName}`, {encoding:'ascii'})
   const words = contents.split("\n");
 
-  // Clean out casing and whitespace bug
+  // Start by correcting any casing or whitespace bugs:
   const cleanedWords = words.map((word) => word.trim().toLowerCase());
-  const filteredWords = cleanedWords.filter(word) => (
+  
+  // Reject any remaining uncorrectably entries:
+  // (all words must be lower-case ASCII alphabetic characters, non-blank.)
+  const filteredWords = words.filter((word) => !!word.match(/^[a-z]+$/i));
+  
+  // Filter for distinct works:
   const distinctWords = Array.from(new Set(cleanedWords));
+  
+  // ...And alphabetize them:
   const sortedWords = distinctWords.sort();
-  });
+  
+  // construct our new file:
+  const newContents = sortedWords.join('\n');
+  
+  if(contents === newContents) {
+    console.log(`Nothing to change in ${fileName}`);
+  } else {
+    fs.writeFileSync(__dirname + `${fileName}`, newContents, {encoding:'ascii'});
+    
+    const filteredCount = cleanedWords.length - filteredWords.length;
+    const distinctCount = distinctWords.length - filteredWords.length;
+    console.log("Changes made to normalize file:", fileName);
+    console.log(`Invalid entries removed: ${filteredCount}`);
+    console.log(`Duplicate entries removed: ${distinctCount}`);
+    console.log(`File has been alphabetized and trimmed.`);
+  }
 }
