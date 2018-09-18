549c114d52f6e11bf021776d6de2156fcf8b83f6
Glitch (friendly-words) <none>
2018-09-18 16:24:33 +0000

    👶📖 Checkpoint ./server.js:8/1574 ./package.json:8/1292

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const sampleSize = require('lodash.samplesize');
 
 // Host our static site, thereby providing our html, css, and js.
 app.use(express.static('public'));
@@ -10,13 +11,31 @@ const getWords = (fileName) => {
   const fileContents = fs.readFileSync(__dirname + `/words/${fileName}`, {encoding:'ascii'})
   return fileContents.split("\n");
 }
-const objects = getWords('objects.txt');
-const predicates = getWords('predicates.txt');
+const OBJECTS = getWords('objects.txt');
+const PREDICATES = getWords('predicates.txt');
 
-console.log(objects, predicates); 
-app.get('/word-pairs/', ()=>{});
-app.get('/objects/', ()=>{});
-app.get('/predicates/', ()=>{});
+const sample = (words) => {
+  return sampleSize(words, 10);
+}
+
+const pairs = (firstWords, secondWords) => {
+  if(firstWords.length !== secondWords.length) {
+    console.error("Word pair collection lengths must match.");
+    return null;
+  }
+  
+}
+
+app.get('/word-pairs/', (req, res)=>{
+  res.sendJSON(pairs(sample(PREDICATES), sample(OBJECTS)));
+});
+app.get('/objects/', (req,res)=>{
+  res.sendJSON(sample(OBJECTS));
+});
+
+app.get('/predicates/', (req,res)=>{
+  res.sendJSON(sample(PREDICATES));
+});
 
 
 // listen for requests :)
