e8acf2f2d53dfae66cedd9e743d1ffe1fde3fa9b
Glitch (friendly-words) <none>
2018-09-18 20:16:13 +0000

    🌠🕴 Checkpoint ./words/normalize.js:8/534 ./words/objects.txt:8/19 ./.glitch_track_file_changes:8/277

--- words/normalize.js
+++ words/normalize.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const Set = Set; // <--
 
 console.log("Starting Normalize");
-for(let fileName of ["objects.txt", "predicates.txt"]) {
+
+["objects.txt", "predicates.txt"].forEach((fileName) => {
   console.log("Normalizing:", fileName);
-  const contents = fs.readFileSync(__dirname + `${fileName}`, {encoding:'ascii'})
+  const contents = fs.readFileSync(__dirname + `/${fileName}`, {encoding:'ascii'})
   const words = contents.split("\n");
 
   // Start by correcting any casing or whitespace bugs:
@@ -15,7 +15,7 @@ for(let fileName of ["objects.txt", "predicates.txt"]) {
   const filteredWords = words.filter((word) => !!word.match(/^[a-z]+$/i));
   
   // Filter for distinct works:
-  const distinctWords = Array.from(new Set(cleanedWords));
+  const distinctWords = Array.from(new Set(filteredWords));
   
   // ...And alphabetize them:
   const sortedWords = distinctWords.sort();
@@ -24,15 +24,18 @@ for(let fileName of ["objects.txt", "predicates.txt"]) {
   const newContents = sortedWords.join('\n');
   
   if(contents === newContents) {
-    console.log(`Nothing to change in ${fileName}`);
-  } else {
-    fs.writeFileSync(__dirname + `${fileName}`, newContents, {encoding:'ascii'});
-    
-    const filteredCount = cleanedWords.length - filteredWords.length;
-    const distinctCount = distinctWords.length - filteredWords.length;
-    console.log("Changes made to normalize file:", fileName);
-    console.log(`Invalid entries removed: ${filteredCount}`);
-    console.log(`Duplicate entries removed: ${distinctCount}`);
-    console.log(`File has been alphabetized and trimmed.`);
+    console.log(`Nothing to change.`);
+    return;
   }
-}
+
+  fs.writeFileSync(__dirname + `/${fileName}`, newContents, {encoding:'ascii'});
+    
+  const filteredCount = cleanedWords.length - filteredWords.length;
+  const distinctCount = distinctWords.length - filteredWords.length;
+  console.log("Changes applied.");
+  console.log(`Invalid entries removed: ${filteredCount}`);
+  console.log(`Duplicate entries removed: ${distinctCount}`);
+  console.log(`File has been alphabetized and trimmed.`);
+});
+                                          
+console.log("All done.");
