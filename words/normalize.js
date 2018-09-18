2a82a32954d608b3792af4122064b3f7da1d47ba
Glitch (friendly-words) <none>
2018-09-18 19:54:35 +0000

    💘😄 Checkpoint ./words/normalize.js:8/943

--- words/normalize.js
+++ words/normalize.js
@@ -5,5 +5,11 @@ for(let fileName of ["objects.txt", "predicates.txt"]) {
   console.log("Normalizing:", fileName);
   const contents = fs.readFileSync(__dirname + `${fileName}`, {encoding:'ascii'})
   const words = contents.split("\n");
-  const cleanedWords = words.map((word) => (word.toLower()))
+
+  // Clean out casing and whitespace bug
+  const cleanedWords = words.map((word) => word.trim().toLowerCase());
+  const filteredWords = cleanedWords.filter(word) => (
+  const distinctWords = Array.from(new Set(cleanedWords));
+  const sortedWords = distinctWords.sort();
+  });
 }
