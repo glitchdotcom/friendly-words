dfe8d0ffec646a104cd9227621fac396850560e3
Glitch (friendly-words) <none>
2018-09-18 20:37:05 +0000

    🍥🔑 Checkpoint ./words/objects.txt:8/18 ./public/client.js:8/159

--- public/client.js
+++ public/client.js
@@ -1,14 +1,9 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
-console.log('hello world :o');
-
 function getWords() {
-  for(let category of ["predicates","objects","word-pairs"]) {
+  ["predicates","objects","word-pairs"].forEach((category) => {
     $.get("/"+category).then((data) => {
       $("#"+category).text(data.join("\n"));
     });
-  }
+  });
 }
 
 // On page load, get some words:
