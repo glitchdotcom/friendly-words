9265927d8136ceb00a6bcc7c348b5cab086eb657
Glitch (friendly-words-again) <none>
2018-09-20 19:03:01 +0000

    💃🚝 Checkpoint ./index.js:9/1862

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const sampleSize = require('lodash.samplesize');
+
+// CORS - Allow pages from any domain to make requests to our API
+app.use(function(request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  return next();
+});
+
+// Load the words into memory:
+const getWords = (fileName) => {
+  const fileContents = fs.readFileSync(__dirname + `/words/${fileName}`, {encoding:'ascii'})
+  return fileContents.split("\n");
+}
+const OBJECTS = getWords('objects.txt');
+const PREDICATES = getWords('predicates.txt');
+
+const sample = (words) => {
+  return sampleSize(words, 10);
+}
+
+const pairs = (firstWords, secondWords) => {
+  if(firstWords.length !== secondWords.length) {
+    console.error("Word pair collection lengths must match.");
+    return null; 
+  }
+  
+  const pairedWords = firstWords.map(
+    (firstWord, index) => (`${firstWord}-${secondWords[index]}`)
+  );
+  
+  return pairedWords;
+}
+
+app.get('/word-pairs/', (req, res)=>{
+  res.json(pairs(sample(PREDICATES), sample(OBJECTS)));
+});
+app.get('/objects/', (req,res)=>{
+  res.json(sample(OBJECTS));
+});
+
+app.get('/predicates/', (req,res)=>{
+  res.json(sample(PREDICATES));
+});
